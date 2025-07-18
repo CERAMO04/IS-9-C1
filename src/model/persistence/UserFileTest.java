d12c19a34948f948dfd9a092f2f3fda6846140a3
Uraviry <ricardoandres1712@gmail.com>
2025-07-17 21:27:24 -0400

    JUnit añadido

---
+++ src/model/persistence/UserFileTest.java
@@ -0,0 +1,15 @@
+package model.persistence;
+
+import model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+public class UserFileTest {
+}
